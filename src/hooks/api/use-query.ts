0f1de02804cd10ccadb937ca3e8ba16281d3235e
raon-kiwoong <89957774+raon-kiwoong@users.noreply.github.com>
2025-08-28 13:14:16 +0900

    feat: use-query업데이트

--- src/hooks/api/use-query.ts
+++ src/hooks/api/use-query.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import useRequest from './use-request';
+import formatUtils from '@utils/format-utils';
 
 const useMyQuery = <T>(
   {
@@ -15,15 +16,36 @@ const useMyQuery = <T>(
 ) => {
   const requestFn = useRequest();
 
-  const { data, isLoading, error, isError, refetch } = useQuery({
+  const {
+    data,
+    isLoading,
+    error,
+    isError,
+    refetch,
+    isFetching,
+    dataUpdatedAt,
+  } = useQuery({
     queryKey: [type, url, JSON.stringify(params)],
-    queryFn: async () => requestFn<T>({ url, type, params }),
+    queryFn: async () =>
+      requestFn<T>({
+        url,
+        type,
+        params: params ? formatUtils.camelToSnakeObjectKey(params) : {},
+      }),
     retry: false,
     refetchOnWindowFocus: false,
     enabled: options?.disabled !== true,
   });
 
-  return { data, isLoading, error, isError, refetch };
+  return {
+    data,
+    isLoading,
+    error,
+    isError,
+    refetch,
+    isFetching,
+    dataUpdatedAt,
+  };
 };
 
 export default useMyQuery;
